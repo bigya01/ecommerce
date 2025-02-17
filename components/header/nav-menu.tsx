89254209ac44ad7d33c9b0df53e1d2533e98ddc3
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-02-17 14:04:21 +0545

    feat: add navigation menu component with category fetching

---
+++ components/header/nav-menu.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+// import { Icons } from "@/components/icons"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export function NavMenu() {
+  const [clicked, setClicked] = useState(false);
+  const [categories, setCategories] = useState([]);
+  console.log(categories);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://dummyjson.com/products/category-list"
+      );
+      setCategories(response.data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Categories</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <div className="w-[800px] grid grid-cols-4 gap-4 p-2">
+              {categories.map((category) => (
+                <Link
+                  key={category}
+                  className="p-2 capitalize cursor-pointer hover:bg-gray-100 rounded-md"
+                  href={`/products/${category}`}
+                >
+                  {category}
+                </Link>
+              ))}
+            </div>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Brands</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <NavigationMenuLink>Link</NavigationMenuLink>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
--- components/sections/header.tsx
+++ components/sections/header.tsx
@@ -1,16 +1,13 @@
-import { User, ShoppingCart, Heart } from "lucide-react";
-import { Input } from "@/components/ui/input";
+import { ShoppingCart, Heart } from "lucide-react";
 import Link from "next/link";
 import { ModeToggle } from "../toggle";
 import { Search } from "../header/search";
+import { NavMenu } from "../header/nav-menu";
 
 export function Header() {
   return (
-    <div className="h-16 border-b-2 flex justify-between p-4">
-      <div className="flex gap-x-8 w-[350px]">
-        <div>Categories</div>
-        <div>Brands</div>
-      </div>
+    <div className=" border-b-2 flex justify-between  items-center p-4">
+      <NavMenu/>
       <Link href="/products" className="font-bold text-2xl">nepmart</Link>
       <div className="flex gap-x-4">
         <Search />
